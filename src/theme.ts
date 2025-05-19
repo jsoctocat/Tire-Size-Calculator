dfac3db6f3d45d0f0c4ffe290bf9843543fe70c0
jsoctocat <42134925+jsoctocat@users.noreply.github.com>
2025-05-18 20:58:19 -0700

    Update theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,3 @@
-import { createSystem, defineConfig, defineRecipe, defaultConfig } from '@chakra-ui/react';
-
-const buttonRecipe = defineRecipe({
-    base: {
-        color: "blue.500",
-        bg: "transparent",
-        border: "40px solid",
-        borderColor: "white",
-    }
-  })
-  
-const customConfig = defineConfig({
-    theme: {
-        recipes: {
-        button: buttonRecipe,
-        },
-    },
-})
+import { createSystem, defaultConfig } from '@chakra-ui/react';
 
 export const system = createSystem(defaultConfig/*, customConfig*/)
