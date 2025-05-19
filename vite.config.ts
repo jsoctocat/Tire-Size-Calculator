79a8649982d1447711ecef534135b35bb48d056c
jsoctocat <42134925+jsoctocat@users.noreply.github.com>
2025-05-18 21:05:59 -0700

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"Tire-Size_calculator/",
+  base:"Tire-Size-Calculator/",
 })
